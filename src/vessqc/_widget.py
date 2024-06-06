88c2309eeab6e23f39d21574543ff8149d02deac
lampen <lampen@isas.de>
2024-06-06 11:41:37 +0200

    New version of button_clicked()

--- src/vessqc/_widget.py
+++ src/vessqc/_widget.py
@@ -88,23 +88,18 @@ class VessQC(QWidget):
 
         # Load the data files
         image_data = sitk.ReadImage(image_file)
-        image = sitk.GetArrayFromImage(image_data)
-        print('Image')
-        self.show_info(image)
+        self.image = sitk.GetArrayFromImage(image_data)
+        # self.show_info(self.image)
 
         uncertainty_data = sitk.ReadImage(uncertainty_file)
         self.uncertainty = sitk.GetArrayFromImage(uncertainty_data)
-        self.uncert_values, counts = np.unique(self.uncertainty, \
-            return_counts=True)
-        print('num uncert', np.size(self.uncert_values))
-        print(self.uncert_values)
-        print(counts)
+        self.uncert_values = np.unique(self.uncertainty)
 
         prediction_data = sitk.ReadImage(prediction_file)
         self.prediction = sitk.GetArrayFromImage(prediction_data)
 
         # Show the image in Napari
-        self.viewer.add_image(image, name='Data')
+        self.viewer.add_image(self.image, name='Data')
         self.viewer.dims.ndisplay=3
 
     def btn_segmentation(self):
@@ -121,22 +116,32 @@ class VessQC(QWidget):
         # Define a group box inside the uncertanty list
         group_box = QGroupBox('Uncertainty List')
 
-        # Define some buttons
-        button1 = QPushButton('Area 1')
-        button1.clicked.connect(self.button_clicked)
-        button2 = QPushButton('Area 2')
-        button2.clicked.connect(self.button_clicked)
-        button3 = QPushButton('Area 51')
-        button3.clicked.connect(self.button_clicked)
+        # Define some buttons and select a few uncertainty values for the labels
+        button5 = QPushButton('Area 5')
+        button5.clicked.connect(self.button_clicked)
+        value5 = self.uncert_values[5]
+        label5 = QLabel(str(value5))
+
+        button8 = QPushButton('Area 8')
+        button8.clicked.connect(self.button_clicked)
+        value8 = self.uncert_values[8]
+        label8 = QLabel(str(value8))
+
+        button12 = QPushButton('Area 12')
+        button12.clicked.connect(self.button_clicked)
+        value12 = self.uncert_values[12]
+        label12 = QLabel(str(value12))
 
         # add widgets to the group box
         self.group_box_layout = QGridLayout()
-        self.group_box_layout.addWidget(button1, 0, 0)
-        self.group_box_layout.addWidget(QLabel('123.45'), 0, 1)
-        self.group_box_layout.addWidget(button2, 1, 0)
-        self.group_box_layout.addWidget(QLabel('234.56'), 1, 1)
-        self.group_box_layout.addWidget(button3, 2, 0)
-        self.group_box_layout.addWidget(QLabel('345.67'), 2, 1)
+        self.group_box_layout.addWidget(QLabel('Area'), 0, 0)
+        self.group_box_layout.addWidget(QLabel('Uncertainty'), 0, 1)
+        self.group_box_layout.addWidget(button5, 1, 0)
+        self.group_box_layout.addWidget(label5, 1, 1)
+        self.group_box_layout.addWidget(button8, 2, 0)
+        self.group_box_layout.addWidget(label8, 2, 1)
+        self.group_box_layout.addWidget(button12, 3, 0)
+        self.group_box_layout.addWidget(label12, 3, 1)
         group_box.setLayout(self.group_box_layout)
 
         uncertainty_list_layout = QVBoxLayout()
@@ -147,11 +152,17 @@ class VessQC(QWidget):
         self.uncertainty_list.show()
 
     def button_clicked(self):
-        text1 = self.sender().text()
-        # print(self.group_box_layout.indexOf(self.sender()))
-        index = self.group_box_layout.indexOf(self.sender())
-        text2 = self.group_box_layout.itemAt(index + 1).widget().text()
-        print(text2)
+        # index = self.group_box_layout.indexOf(self.sender())
+        # text2 = self.group_box_layout.itemAt(index + 1).widget().text()
+
+        text1 = self.sender().text()        # name of the button: Area nn
+        index = int(text1[5:])              # number of the area
+        value = self.uncert_values[index]   # uncertainty value of the area
+
+        indices = np.where(self.uncertainty == value)   # find all data points
+        area = np.zeros(self.uncertainty.shape, dtype=np.int8)
+        area[indices] = index + 1           # build a new label layer
+        self.viewer.add_labels(area, name=text1)
 
     def show_info(self, image):
         print('type',  type(image))
@@ -169,5 +180,5 @@ class VessQC(QWidget):
 
     def on_close(self):
         print("on_close")
-        if hasattr(self, uncertainty_list):
+        if hasattr(self, 'uncertainty_list'):
             self.uncertainty_list.close()
