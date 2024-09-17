c63496e66dadbabba57653b9f88fa19c8368913b
lampen <lampen@isas.de>
2024-09-17 17:48:04 +0200

    New functions test_build_areas() and test_btn_uncertainty()

--- src/vessqc/_tests/test_widget.py
+++ src/vessqc/_tests/test_widget.py
@@ -1,40 +1,48 @@
 import pytest
 import numpy as np
+import PyQt5
 from unittest.mock import patch
 from pathlib import Path
 from tifffile import imread
 from vessqc._widget import VessQC
-
-# make_napari_viewer is a pytest fixture that returns a napari viewer object
-# you don't need to import it, as long as napari is installed
-# in your testing environment
 # capsys is a pytest fixture that captures stdout and stderr output streams
 
-parent = Path(__file__).parent / "Data"
-image_path =  parent / "Image.tif"
+
+# Class to save and share variables between tests
+class ValueStorage():
+    parent = Path(__file__).parent / "Data"
+    image_path =  parent / "Image.tif"
+    areas = None
+
 
 # define a fixture for the image
 @pytest.fixture
 def image_data():
+    image_path = ValueStorage.image_path
     return imread(image_path)
 
 @pytest.fixture
 def prediction_data():
+    parent = ValueStorage.parent
     prediction_file = parent / 'Prediction.tif'
     return imread(prediction_file)
 
 @pytest.fixture
 def uncertainty_data():
+    parent = ValueStorage.parent
     uncertainty_file = parent / 'Uncertainty.tif'
     return imread(uncertainty_file)
 
-# define a fixture for the Napari viewer and the VessQC object
+# make_napari_viewer is a pytest fixture that returns a napari viewer object
+# you don't need to import it, as long as napari is installed
+# in your testing environment
 @pytest.fixture
 def vess_qc(make_napari_viewer):
     viewer = make_napari_viewer()
     return VessQC(viewer)           # create a VessQC object and give it back
 
 
+@pytest.mark.init
 def test_init(vess_qc):
     assert str(type(vess_qc)) == "<class 'vessqc._widget.VessQC'>"
     assert vess_qc.start_multiple_viewer == True
@@ -44,9 +52,10 @@ def test_init(vess_qc):
 # Durch den Patch wird die Funktion getOpenFileName durch die Return-Values
 # ersetzt
 @patch("qtpy.QtWidgets.QFileDialog.getOpenFileName",
-    return_value=(image_path, None))
+    return_value=(ValueStorage.image_path, None))
 
 
+@pytest.mark.load
 def test_btn_load(mock_get_open_file_name, vess_qc, image_data):
     viewer = vess_qc.viewer
     vess_qc.btn_load()
@@ -56,14 +65,15 @@ def test_btn_load(mock_get_open_file_name, vess_qc, image_data):
     layer = viewer.layers[0]
     assert layer.name == 'Image'
     assert np.array_equal(layer.data, image_data)
-    assert vess_qc.parent == parent
+    assert vess_qc.parent == ValueStorage.parent
     assert vess_qc.suffix == '.tif'
 
 
+@pytest.mark.segmentation
 def test_btn_segmentation(vess_qc, prediction_data, uncertainty_data):
     viewer = vess_qc.viewer
     vess_qc.suffix = '.tif'
-    vess_qc.parent = parent
+    vess_qc.parent = ValueStorage.parent
     vess_qc.is_tifffile = True
     vess_qc.areas = [None]
     vess_qc.btn_segmentation()
@@ -75,3 +85,30 @@ def test_btn_segmentation(vess_qc, prediction_data, uncertainty_data):
     assert layer1.name == 'Uncertainty'
     assert np.array_equal(layer0.data, prediction_data)
     assert np.array_equal(layer1.data, uncertainty_data)
+
+
+@pytest.mark.build_areas
+def test_build_areas(vess_qc, uncertainty_data):
+    # (17.09.2024)
+    vess_qc.uncertainty = uncertainty_data
+    vess_qc.build_areas()
+    ValueStorage.areas = vess_qc.areas
+
+    assert len(vess_qc.areas) == 10
+    assert vess_qc.areas[1]['name'] == 'Area 1'
+    assert vess_qc.areas[2]['unc_value'] == np.float32(0.2)
+    assert vess_qc.areas[3]['counts'] == 34
+    assert vess_qc.areas[4]['centroid'] == ()
+    assert vess_qc.areas[5]['where'] == None
+    assert vess_qc.areas[6]['done'] == False
+
+
+@pytest.mark.uncertainty
+def test_btn_uncertainty(vess_qc):
+    # (17.09.2024)
+    vess_qc.areas = ValueStorage.areas
+    vess_qc.btn_uncertainty()
+
+    assert str(type(vess_qc.popup_window)) == "<class 'PyQt5.QtWidgets.QWidget'>"
+    assert vess_qc.popup_window.windowTitle() == 'napari'
+    #assert vess_qc.popup_window.minimumSize == PyQt5.QtCore.QSize(350, 300)
