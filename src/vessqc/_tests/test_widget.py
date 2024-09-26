2bfd06b5f838d26a2c2fa5afcecd5eb3e39eb2cb
lampen <lampen@isas.de>
2024-09-26 18:15:44 +0200

    Press button 'restore'

--- src/vessqc/_tests/test_widget.py
+++ src/vessqc/_tests/test_widget.py
@@ -1,6 +1,6 @@
 import pytest
-import numpy as np
 import napari
+import numpy as np
 import qtpy
 from qtpy.QtWidgets import QGridLayout
 from qtpy.QtTest import QTest
@@ -16,48 +16,53 @@ class ValueStorage():
     parent = Path(__file__).parent / "Data"
     image_path =  parent / "Image.tif"
 
-
-# define a fixture for the image
-@pytest.fixture
-def image_data():
-    return imread(ValueStorage.image_path)
-
-@pytest.fixture
-def prediction_data():
-    prediction_file = ValueStorage.parent / 'Prediction.tif'
-    return imread(prediction_file)
-
-@pytest.fixture
-def prediction_new():
-    # (24.09.2024)
-    prediction_new_file = ValueStorage.parent / 'Prediction_new.tif'
-    return imread(prediction_new_file)
-
-@pytest.fixture
-def uncertainty_data():
-    uncertainty_file = ValueStorage.parent / 'Uncertainty.tif'
-    return imread(uncertainty_file)
-
-@pytest.fixture
-def area5_data():
-    # (20.09.2024)
-    area5_file = ValueStorage.parent / 'Area5.tif'
-    return imread(area5_file)
-
-@pytest.fixture
-def area5_new():
-    # (24.09.2024)
-    area5_new_file = ValueStorage.parent / 'Area5_new.tif'
-    return imread(area5_new_file)
-
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
-# you don't need to import it, as long as napari is installed
-# in your testing environment
+# you don't need to import it, as long as napari is installed in your
+# testing environment
 @pytest.fixture
 def vessqc(make_napari_viewer):
     # (12.09.2024)
     viewer = make_napari_viewer()
     return VessQC(viewer)           # create a VessQC object and give it back
+
+# define fixtures for the image data
+@pytest.fixture
+def image_data():
+    filename = ValueStorage.parent / "Image.tif"
+    return imread(filename)
+
+@pytest.fixture
+def prediction_data():
+    filename = ValueStorage.parent / 'Prediction.tif'
+    return imread(filename)
+
+@pytest.fixture
+def prediction_new_data():
+    # (24.09.2024)
+    filename = ValueStorage.parent / 'Prediction_new.tif'
+    return imread(filename)
+
+@pytest.fixture
+def uncertainty_data():
+    filename = ValueStorage.parent / 'Uncertainty.tif'
+    return imread(filename)
+
+@pytest.fixture
+def uncertainty_new_data():
+    filename = ValueStorage.parent / 'Uncertainty_new.tif'
+    return imread(filename)
+
+@pytest.fixture
+def area5_data():
+    # (20.09.2024)
+    filename = ValueStorage.parent / 'Area5.tif'
+    return imread(filename)
+
+@pytest.fixture
+def area5_new_data():
+    # (24.09.2024)
+    filename = ValueStorage.parent / 'Area5_new.tif'
+    return imread(filename)
 
 @pytest.fixture
 def areas(vessqc, uncertainty_data):
@@ -75,8 +80,7 @@ def test_init(vessqc):
     assert vessqc.save_uncertainty == False
 
 
-# Durch den Patch wird die Funktion getOpenFileName durch die Return-Values
-# ersetzt
+# The patch replaces the getOpenFileName() function with the return values
 @patch("qtpy.QtWidgets.QFileDialog.getOpenFileName",
     return_value=(ValueStorage.image_path, None))
 @pytest.mark.load
@@ -247,64 +251,76 @@ def get_grid_layout(vessqc: VessQC) -> QGridLayout:
 
 @patch("qtpy.QtWidgets.QWidget.show")
 @pytest.mark.transfer
-def test_transfer(mock_widget_show, vessqc, areas, prediction_data,
-    uncertainty_data, area5_new):
+def test_transfer(mock_widget_show, vessqc, prediction_data, prediction_new_data,
+    uncertainty_data, uncertainty_new_data, areas, area5_new_data):
     # (24.09.2024)
     vessqc.prediction = prediction_data
     vessqc.uncertainty = uncertainty_data
-    vessqc.viewer.add_labels(vessqc.prediction, name='Prediction')
-    vessqc.viewer.add_image(vessqc.uncertainty, name='Uncertainty')
+    pred_layer = vessqc.viewer.add_labels(vessqc.prediction, name='Prediction')
+    unc_layer =  vessqc.viewer.add_image(vessqc.uncertainty, name='Uncertainty')
     vessqc.areas = areas
 
-    # search for the button "Area 5"
+    # search for the row with button "Area 5"
     grid_layout = get_grid_layout(vessqc)
-    count = grid_layout.rowCount()
+    n = grid_layout.rowCount()
     index1 = None
-    for i in range(count):
-        item0 = grid_layout.itemAtPosition(i, 0)
-        if str(type(item0.widget())) == "<class 'PyQt5.QtWidgets.QPushButton'>":
-            if item0.widget().text() == 'Area 5':
-                index1 = i
+    for i in range(n):
+        widget0 = grid_layout.itemAtPosition(i, 0).widget()
+        if str(type(widget0)) == "<class 'PyQt5.QtWidgets.QPushButton'>" and \
+            widget0.text() == 'Area 5':
+            index1 = i
+            break
+
     assert index1 != None       # button "Area 5" has been found
 
-    # press the button "Area 5" and call show_area()
-    button51 = grid_layout.itemAtPosition(index1, 0).widget()
-    QTest.mouseClick(button51, Qt.LeftButton)
+    # press the button "Area 5" to call "show_area()"
+    QTest.mouseClick(widget0, Qt.LeftButton)
 
-    # search for the Napari layer "Area 5"
+    # search for the Napari layer "Area 5" and change this data
     if any(layer.name == 'Area 5' and isinstance(layer, napari.layers.Labels)
         for layer in vessqc.viewer.layers):
-        layer5 = vessqc.viewer.layers['Area 5']
-        assert layer5.name == 'Area 5'
-        layer5.data = area5_new
+        area5_layer = vessqc.viewer.layers['Area 5']
+        assert area5_layer.name == 'Area 5'
+        area5_layer.data = area5_new_data       # replace the data of the layer
     else:
         assert False
 
-    # press the button done in row "Area 5" and call compare_and_transfer()
-    item3 = grid_layout.itemAtPosition(index1, 3)
-    btn_done = item3.widget()
-    assert btn_done.text() == 'done'
-    QTest.mouseClick(btn_done, Qt.LeftButton)
+    # press the button done in row "Area 5" to call "compare_and_transfer()"
+    widget3 = grid_layout.itemAtPosition(index1, 3).widget()
+    assert str(type(widget3)) == "<class 'PyQt5.QtWidgets.QPushButton'>"
+    assert widget3.text() == 'done'
+    QTest.mouseClick(widget3, Qt.LeftButton)
 
-    # find the new position of the Button "Area 5"
+    # the data in the Napari layers Prediction and Uncertainty should have
+    # been changed by the function compare_and_transfer()
+    assert np.array_equal(pred_layer.data, prediction_new_data)
+    assert np.array_equal(unc_layer.data, uncertainty_new_data)
+    assert areas[5]['done'] == True
+
+    # find the new row of the button "Area 5"
     grid_layout = get_grid_layout(vessqc)
-    count = grid_layout.rowCount()
+    n = grid_layout.rowCount()
     index2 = None
-    for i in range(count):
-        item0 = grid_layout.itemAtPosition(i, 0)
-        if str(type(item0.widget())) == "<class 'PyQt5.QtWidgets.QPushButton'>":
-            if item0.widget().text() == 'Area 5':
-                index2 = i
+    for i in range(n):
+        widget0 = grid_layout.itemAtPosition(i, 0).widget()
+        if str(type(widget0)) == "<class 'PyQt5.QtWidgets.QPushButton'>" and \
+            widget0.text() == 'Area 5':
+            index2 = i
+            break
 
     assert index2 != None       # button "Area 5" has been found
     assert index2 > index1      # "Area 5" is now at the end of the list
-    assert areas[5]['done'] == True
-
-    # the second button is changed from 'done' to 'restore'
-    button52 = grid_layout.itemAtPosition(index2, 3).widget()
-    assert button52.text() == 'restore'
+    assert widget0.isEnabled() == False     # the button is inactive
 
     # the Napari layer "Area 5" is removed
     if any(layer.name == 'Area 5' and isinstance(layer, napari.layers.Labels)
         for layer in vessqc.viewer.layers):
         assert False
+
+    # press the button restore to call the function restore()
+    widget3 = grid_layout.itemAtPosition(index2, 3).widget()
+    assert str(type(widget3)) == "<class 'PyQt5.QtWidgets.QPushButton'>"
+    assert widget3.text() == 'restore'
+    QTest.mouseClick(widget3, Qt.LeftButton)
+
+    assert areas[5]['done'] == False
